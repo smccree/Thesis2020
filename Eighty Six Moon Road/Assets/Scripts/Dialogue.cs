686ace7fca3d87ee236514cbc3d7beb7247d0db1
Sonya McCree <smccree@bu.edu>
2019-12-02 22:27:08 -0500

    dialogue system ui start
    built simple dialogue system into ui for building upon later

---
+++ Eighty Six Moon Road/Assets/Scripts/Dialogue.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Dialogue
+{
+    //dialogue options for basic dialogue system
+
+    [TextArea(3, 10)]
+
+    public string name; //name of NPC talking to
+    public string[] sentences;
+}
---
+++ Eighty Six Moon Road/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class DialogueManager : MonoBehaviour
+{
+    public TMP_Text nameText;
+    public TMP_Text dialogueText;
+
+    //building simple dialogue system to start - added complexity l8r
+
+    private Queue<string> sentences; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        Debug.Log("Starting conversation with " + dialogue.name);
+
+        nameText.text = dialogue.name;//person's name
+
+        sentences.Clear();
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        Debug.Log(sentence);
+
+        dialogueText.text = sentence;
+    }
+
+    public void EndDialogue()
+    {
+
+    }
+}
